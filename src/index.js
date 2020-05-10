2890b267edb5f673300392aa41df4b9a36eb50ed
db1 <vaCiadb1>
2020-05-10 19:29:24 +0200

    init

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as serviceWorker from './serviceWorker';
+import {MainPageComponent} from "./components/MainPageComponent";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainPageComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
